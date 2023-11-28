40b1fa9f352114e0c1f5818d00da994f19cc4c24
Saransh Mehta <saransh@jify.co>
2023-11-27 17:48:37 -0700

    Commit

---
+++ projectv2/target/debug/deps/projectv2.d
@@ -0,0 +1,5 @@
+C:\Users\saran\OneDrive\Desktop\Academics\UofA\Fall '23\ECE 522\Project\ece-522-project\projectv2\target\debug\deps\projectv2.exe: src\main.rs
+
+C:\Users\saran\OneDrive\Desktop\Academics\UofA\Fall '23\ECE 522\Project\ece-522-project\projectv2\target\debug\deps\projectv2.d: src\main.rs
+
+src\main.rs:
